b34ccb3ce3222f7c0d60f674cdb351d0887c6fd5
ZHAOWEN LONG <ZLONG003@E.NTU.EDU.SG>
2020-06-28 21:55:05 +0800

    Added second trial

--- RTesting.R
+++ RTesting.R
@@ -1,2 +1,4 @@
 print("this file is created within RStudio")
 print("And how it's live on Github")
+print("Codes I learnt in R so far")
+print("version, install.packages, library(), sessionInfo, etc.")
