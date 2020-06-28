7fad2cd0b6da0ca08e002e61581d3a0dc2cb229a
ZHAOWEN LONG <ZLONG003@E.NTU.EDU.SG>
2020-06-28 22:01:06 +0800

    3rd trial, since 2nd didn't reflect in Github acct

--- RTesting.R
+++ RTesting.R
@@ -2,3 +2,10 @@ print("this file is created within RStudio")
 print("And how it's live on Github")
 print("Codes I learnt in R so far")
 print("version, install.packages, library(), sessionInfo, etc.")
+installed.packages
+library(ggplot2)
+example<-mpg
+View(example)
+??DBI
+detach("package:ggplot2", unload = TRUE)
+
